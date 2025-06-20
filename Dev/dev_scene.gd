38131fc93ff5415198199c7193240b3466b43368
Félix Dumas-Lavoie <felixdumaslavoie@gmail.com>
2025-06-20 15:08:24 -0400

    Avancees dans le gui

--- Dev/dev_scene.gd
+++ Dev/dev_scene.gd
@@ -4,10 +4,13 @@ var instances = {}
 
 # Constructor
 func _init():
+	Engine.max_fps = 60
 	var client : ClientNode =  load("res://Client/client.tscn").instantiate()
 	var serveur : ServerNode = load("res://Serveur/serveur.tscn").instantiate()
+	
 	add_child(client)
 	add_child(serveur)
+	
 	instances = {
 		"serveur" : serveur, 
 		"client" : client
@@ -20,5 +23,9 @@ func _process(delta: float) -> void:
 	ImGui.End()
 	ImGui.Begin("Peers")
 	for peer in instances["serveur"].get_peers():
-		ImGui.Text(str(peer["id"]) + " " + str(peer["socket"].get_packet_ip()) + ":"+ str(peer["socket"].get_local_port()))
+		
+		if ImGui.CollapsingHeader( "Client " + str(peer["id"]) ):
+			ImGui.LabelText(  str(peer["socket"].get_packet_ip()) + ":"+ str(peer["socket"].get_local_port()), "socket: ")
+			ImGui.LabelText(  "Lorem ipsum", "Position (x,y) " + ":" )
+		
 	ImGui.End()
